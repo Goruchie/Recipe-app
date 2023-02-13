6576c09fd7518ed2f89e7f9c0ed53ba55a26569a
lana <diegogarciaq98@gmail.com>
2023-02-13 15:11:44 -0500

    Add root route to users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "home#index"
+  root "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
