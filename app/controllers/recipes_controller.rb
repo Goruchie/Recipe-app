365098b9f8a503915ce6ed9c22fb9df1d5efd0a3
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-14 18:33:06 +0000

    Start building show page for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,32 @@ class RecipesController < ApplicationController
     def index
         @recipes = Recipe.all
     end
+
+    def new
+        @recipe = Recipe.new
+    end
+    
+    def create
+        @recipe = Recipe.new(recipe_params)
+        @recipe.user_id = current_user
+        return unless @recipe.save!
+        flash.now[:notice] = 'Recipe created successfully'
+        redirect_to recipes_path
+    end
+
+    def destroy
+        @recipe = Recipe.find(params[:id])
+        # @user = @post.author_id
+        @recipe.destroy
+        redirect_to recipes_path
+    end
+
+    def show
+        @recipe = Recipe.find(params[:id])
+        # @recipe_food = RecipeFood.includes(:user => @recipe.user_id).where(food_id: , recipe_id: @recipe)
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :foods, only: [:new, :create, :destroy]
-  resources :recipes, only: [:index, :new, :show, :destroy]
+  resources :recipes, only: [:index, :new, :create, :show]
+
+  delete '/recipes/:id', to: 'recipes#destroy', as: 'delete_recipe'
 
   devise_for :users
   root to: "foods#index"
