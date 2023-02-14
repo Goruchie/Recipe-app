f28a8f78d58dca2d943bb8a07fb9eeb60c3cb5aa
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-14 17:06:28 +0000

    Add recipes route using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :foods, only: [:new, :create, :destroy]
+  resources :recipes, only: [:index, :new, :show, :destroy]
 
   devise_for :users
   root to: "foods#index"
