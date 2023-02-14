06ec31cc439dc3d15647dc8b5a8a5e4a649f6295
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-14 17:06:48 +0000

    Add seeds for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Recipe.create(name: "Vanilla Cake", preparation_time: "35 minutes", cooking_time: "25 minutes",
+description: "This vanilla cake batter is strong enough for shaped cakes,
+tiered cakes (see the slight variation in my homemade wedding cake recipe), and holds up beautifully under fondant.
+Use this batter for vanilla cupcakes, Bundt cake, or even piñata cake.
+It\'s classy enough for a wedding celebration, but unassuming enough for a big family dinner.",
+public: false, user: 1)
+Recipe.create(name: "Barbeque Chicken", preparation_time: "2 hours", cooking_time: "3 hour",description:
+"Chicken thighs are baked in a homemade BBQ sauce made with ingredients you probably already have on hand,
+such as ketchup and brown sugar.", user: 1)
+# Recipe.create(name: "Recipe 1", preparation_time: "2 hours", cooking_time: "3 hour",description: "Test", public: false, user: 1)
+# Recipe.create(name: "Recipe 1", preparation_time: "2 hours", cooking_time: "3 hour",description: "Test", public: false, user: 1)
+# Recipe.create(name: "Recipe 1", preparation_time: "2 hours", cooking_time: "3 hour",description: "Test", public: false, user: 1)
