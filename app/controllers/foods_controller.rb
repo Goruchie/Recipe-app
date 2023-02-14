0a177c5b779cf98a266611a3cddcbc36eb00f1a7
Goruchie <marcosgualtero43253@gmail.com>
2023-02-14 11:28:31 -0300

    Structure food list

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,32 @@
+class FoodsController < ApplicationController
+    def index
+        @food = Food.all
+      end
+    
+      def new
+        @food = Food.new
+      end
+    
+      def create
+        @food = Food.new(food_params)
+        @food.id = current_user.id
+        flash.now[:notice] = 'Food created successfully' if @food.save!
+        redirect_to foods_path(current_user.id)
+      end
+    
+      def show
+        @food = Food.find(params[:id])
+        @user = User.find(params[:user_id])
+      end
+    
+      def destroy
+        @food = Food.find(params[:id])
+        @user = @post.author_id
+        @food.destroy
+        redirect_to food_path_path(@user)
+      end
+    
+      def food_params
+        params.require(:food).permit(:name, :measurement_unit, :price)
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :foods, only: [:new, :create, :destroy]
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "users#index"
+  root to: "foods#index"
+  get '/foods', to: 'foods#create', as: 'food_create'
+  
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  
+
 end
