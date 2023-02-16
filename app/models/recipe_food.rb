732f2e10dd1482deb419a0f39e907e2b91e47737
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-16 00:07:28 +0000

    Fix rubocop errors

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,4 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :foods, foreign_key: 'food_id', class_name: 'Food'
-  belongs_to :recipes, foreign_key: 'recipe_id', class_name: 'Recipe'
+  belongs_to :foods, foreign_key: 'food_id', class_name: 'Food' , dependent: :destroy
+  belongs_to :recipes, foreign_key: 'recipe_id', class_name: 'Recipe',  dependent: :destroy
 end
